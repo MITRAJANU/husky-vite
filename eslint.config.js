861b69d5637b1c5e771c339d05ec6f89b923ff3c
MITRAJANU <mitrajanu.mishra@gmail.com>
2025-05-17 13:09:35 +0530

    ADD:Added Husky

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -11,11 +12,23 @@ export default tseslint.config(
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: globals.browser,
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +36,16 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+
+      // Production-grade additions:
+      'react/jsx-uses-react': 'off', // no longer needed with new JSX transform
+      'react/react-in-jsx-scope': 'off', // same as above
+      'react/prop-types': 'off', // using TypeScript, not PropTypes
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': 'off',
     },
   },
 )
