021308d64b0ab97fda2631bd1c5776afb3b324d3
MITRAJANU <mitrajanu.mishra@gmail.com>
2025-05-18 11:07:39 +0530

    CHK:Checking types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 
 function App() {
+  const vary = 'YTY'
   return (
     <>
+      <h1>{vary}</h1>
       <div className="flex justify-center items-center min-h-screen">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
