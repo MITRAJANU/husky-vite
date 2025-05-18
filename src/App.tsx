808c5fcf50d133aa4f1091145bc2315d3c54987a
MITRAJANU <mitrajanu.mishra@gmail.com>
2025-05-18 11:15:02 +0530

    CHK:Checking types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const vary = 'YTY'
+  const [counter, setCounter] = useState(0)
+
+  function increment(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    setCounter(counter + 1)
+  }
+  function decrement(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    setCounter(counter - 1)
+  }
   return (
     <>
-      <h1>{vary}</h1>
       <div className="flex justify-center items-center min-h-screen">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button onClick={increment} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Increment
+        </button>
+        <h1>{counter}</h1>
+        <button onClick={decrement} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4">
+          Decrement
+        </button>
+
+        <div className="flex justify-center items-center min-h-screen">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
       </div>
     </>
   )
